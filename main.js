ce3d9342ca7191edf764ee795f1adb950f6b2072
Agustin Lorenzatti <lorenzatiro@gmail.com>
2023-07-09 10:39:59 -0300

    entrega lista

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const shoppingCart = []
 
-const products = [{ id: 1, img: "Imagenes/7daystodie.jpg" ,name: '7 Days to Die', precio: 2599},
-                  { id: 2, img: "Imagenes/assasingcreed.jpg" ,name: 'Bermuda playera', precio: 3859},
-                  { id: 4, img: "Imagenes/evilwest.jpg" ,name: 'Pantalón de jean', precio: 8599},
-                  { id: 5, img: "Imagenes/farming22.jpg" ,name: 'Camisa Office', precio: 4894},
-                  { id: 6, img: "Imagenes/mountandblade.jpg" ,name: 'Abrigo Gadget', precio: 9748},
-                  { id: 7, img: "Imagenes/residentevil.jpg" ,name: 'Sombrero de gala', precio: 3281},
-                  { id: 8, img: "Imagenes/superhot.jpg" ,name: 'Musculosa runner', precio: 1938},
-                  { id: 9, img: "Imagenes/terraria.jpg" ,name: 'Malla enteriza Lafelí', precio: 3122},
+const products = [{ id: 1, img: "Imagenes/7daystodie.jpg" ,name: "7 Days to Die", price: 279},
+                  { id: 2, img: "Imagenes/assasingcreed.jpg" ,name: "Assasin's Creed Oddyssey", price: 5990},
+                  { id: 4, img: "Imagenes/evilwest.jpg" ,name: "Evil West", price: 6499},
+                  { id: 5, img: "Imagenes/farming22.jpg" ,name: "Farming Simulator 22", price: 3849},
+                  { id: 6, img: "Imagenes/mountandblade.jpg" ,name: "Mount And Blade 2", price: 2999},
+                  { id: 7, img: "Imagenes/residentevil.jpg" ,name: "Residen Evil Village", price: 1700},
+                  { id: 8, img: "Imagenes/superhot.jpg" ,name: "Super Hot", price: 1200},
+                  { id: 9, img: "Imagenes/terraria.jpg" ,name: "Terraria", price: 129},
 ]
 
-const container = document.querySelector('div#container.container')
-const inputSearch = document.querySelector('input#inputSearch')
-/*const btnFinishBuy = document.querySelector("button.finishBuy")*/
+const container = document.querySelector("div#container.container")
+const inputSearch = document.querySelector("input#inputSearch")
+
--- miarchivo.js
+++ miarchivo.js
@@ -1,53 +1,54 @@
 
 
-function retornarCardHTML(products) {
+function returnCardHTML(products) {
   return `<div class="div-card">
-               <div class="prenda">
+               <div class="game">
                <img src="${products.img}" alt="Imagen del producto">
                </div>
-               <div class="prenda">
+               <div class="game">
                   <h3>${products.name}</h3>
                </div>
-               <div class="importe">
-                   <p>$ ${products.precio}</p>
+               <div class="price">
+                   <p>$ ${products.price}</p>
                </div>
-               <div class="comprar"><button class="button button-outline" id="${products.id}">add</button></div>
+               <div class="buy"><button class="button button-outline" id="${products.id}">Agregar al carrito</button></div>
           </div>`
 }
 
 function addProdCart() {
-    const btns = document.querySelectorAll('button.button.button-outline')
+    const btns = document.querySelectorAll("button.button.button-outline")
+    
     btns.forEach((btn)=> {
-        btn.addEventListener('click', ()=> {
+        btn.addEventListener("click", ()=> {
             let product = products.find((products)=> products.id === parseInt(btn.id))
             shoppingCart.push(product)
+            alert(product)
             console.table(shoppingCart)
+            
         })
     })
 }
 
 
-function cargarProductos(array) {
+function addProducts(array) {
   container.innerHTML = ""
   array.forEach((product)=> {
-      container.innerHTML += retornarCardHTML(product)
+      container.innerHTML += returnCardHTML(product)
   })
   addProdCart()
   buy()
 }
 
 
-
-cargarProductos(products)
-
 function buy() {
-  const btn = document.querySelectorAll('button.finishBuy')
+  const btn = document.querySelectorAll("button.finishBuy")
   btn.forEach((button)=> {
-  button.addEventListener('click', ()=> { 
-    (shoppingCart.length > 0) 
+    button.addEventListener("click", ()=> { 
+      (shoppingCart.length > 0) 
       alert ("Usted ha comprado por una valor total de $" + shoppingCart.reduce((acc, products)=> acc + products.precio, 0))
     }) 
-    })
+  })
 }
 
+addProducts(products)
 
